f3319efffe97cec7ce6dc0d262876e840b0b0d76
David Pérez <deividpa022@gmail.com>
2024-09-16 12:49:42 -0500

    feat: Part 6.2 completed

--- unicafe-redux/src/main.jsx
+++ unicafe-redux/src/main.jsx
@@ -1,36 +1,61 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-
+import ReactDOM from 'react-dom'
 import { createStore } from 'redux'
-import reducer from './reducer'
 
-const store = createStore(reducer)
+// Reducer to manage the state of the application
+const initialState = {
+  good: 0,
+  ok: 0,
+  bad: 0
+}
+
+const counterReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'GOOD':
+      return { ...state, good: state.good + 1 }
+    case 'OK':
+      return { ...state, ok: state.ok + 1 }
+    case 'BAD':
+      return { ...state, bad: state.bad + 1 }
+    case 'ZERO':
+      return initialState
+    default:
+      return state
+  }
+}
+
+// Create the store
+const store = createStore(counterReducer)
 
 const App = () => {
-  const good = () => {
-    store.dispatch({
-      type: 'GOOD'
-    })
-  }
+  // Handlers for the buttons
+  const handleGood = () => store.dispatch({ type: 'GOOD' })
+  const handleOk = () => store.dispatch({ type: 'OK' })
+  const handleBad = () => store.dispatch({ type: 'BAD' })
+  const handleReset = () => store.dispatch({ type: 'ZERO' })
 
   return (
     <div>
-      <button onClick={good}>good</button> 
-      <button>ok</button> 
-      <button>bad</button>
-      <button>reset stats</button>
-      <div>good {store.getState().good}</div>
-      <div>ok</div>
-      <div>bad</div>
+      <h1>Give Feedback</h1>
+      <button onClick={handleGood}>good</button>
+      <button onClick={handleOk}>ok</button>
+      <button onClick={handleBad}>bad</button>
+      <button onClick={handleReset}>reset stats</button>
+
+      <h2>Statistics</h2>
+      <p>good: {store.getState().good}</p>
+      <p>ok: {store.getState().ok}</p>
+      <p>bad: {store.getState().bad}</p>
     </div>
   )
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-
 const renderApp = () => {
-  root.render(<App />)
+  ReactDOM.render(<App />, document.getElementById('root'))
 }
 
-renderApp()
+// Suscribe to the store to render the app when the state changes
 store.subscribe(renderApp)
+
+// Initial render
+renderApp()
