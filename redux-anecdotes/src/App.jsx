9a960015abdfeb1b9979807d187924210a6098b0
David Pérez <deividpa022@gmail.com>
2024-09-17 22:52:25 -0500

    Parts 6.3 to 6.8 implemented

---
+++ redux-anecdotes/src/App.jsx
@@ -0,0 +1,13 @@
+import AnecdoteList from './components/AnecdoteList'
+import AnecdoteForm from './components/anecdoteForm'
+
+const App = () => {
+  return (
+    <div>
+      <AnecdoteList />
+      <AnecdoteForm />
+    </div>
+  )
+}
+
+export default App
