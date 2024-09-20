ca5ce0cab0e2d64a835a0962153c6e220587f8cb
David Pérez <deividpa022@gmail.com>
2024-09-19 23:18:52 -0500

    feat: Part 5.12 is now working. Notifications is a part of the global states

--- redux-anecdotes/src/App.jsx
+++ redux-anecdotes/src/App.jsx
@@ -1,10 +1,13 @@
 import AnecdoteList from './components/AnecdoteList'
 import Filter from './components/Filter'
+import Notification from './components/Notification'
 import AnecdoteForm from './components/anecdoteForm'
 
 const App = () => {
   return (
     <div>{}
+      <h2>Annecdotes APP</h2>
+      <Notification />
       <Filter />
       <AnecdoteList />
       <AnecdoteForm />
