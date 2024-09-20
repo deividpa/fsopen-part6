e0d22e97836cea692c2da4af0dc8c70e5670093f
David Pérez <deividpa022@gmail.com>
2024-09-19 22:45:28 -0500

    feat: Part 6.10 implemented. configureStore is now in use

--- redux-anecdotes/src/reducers/filterReducer.js
+++ redux-anecdotes/src/reducers/filterReducer.js
@@ -1,18 +1,16 @@
 // filterReducer.js
-const filterReducer = (state = '', action) => {
-    switch (action.type) {
-        case 'SET_FILTER':
-            return action.payload
-        default:
-            return state
-    }
-}
-  
-export const setFilter = (filter) => {
-    return {
-        type: 'SET_FILTER',
-        payload: filter
-    }
-}
+import { createSlice } from '@reduxjs/toolkit';
 
-export default filterReducer
+const filterSlice = createSlice({
+  name: 'filter',
+  initialState: '',
+  reducers: {
+    setFilter(state, action) {
+      return action.payload;
+    }
+  }
+});
+
+export const { setFilter } = filterSlice.actions;
+
+export default filterSlice.reducer;
